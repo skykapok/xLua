d9682f6441e1e0880cdc0b6c4152435d75c7edc5
skykapok <skykapok@gmail.com>
2019-12-21 13:29:00 +0800

    增加rapidjson支持

--- build/make_win32_lua53.bat
+++ build/make_win32_lua53.bat
@@ -4,4 +4,5 @@ popd
 cmake --build build32 --config Release
 md plugin_lua53\Plugins\x86
 copy /Y build32\Release\xlua.dll plugin_lua53\Plugins\x86\xlua.dll
+copy /Y build32\Release\xlua.dll ..\Assets\Plugins\x86\xlua.dll
 pause
